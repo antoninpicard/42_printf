17c086d74b0df752e317699a20f8a2649c243ff3
Anto <antoninpicard0@gmail.com>
2024-11-14 17:59:22 +0100

    Origami

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,19 +6,34 @@
 /*   By: anpicard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:00:11 by anpicard          #+#    #+#             */
-/*   Updated: 2024/11/14 12:38:27 by anpicard         ###   ########.fr       */
+/*   Updated: 2024/11/14 17:31:16 by anpicard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/ft_printf.h"
+#include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+int    ft_printf(const char *format, ...)
 {
-	(void)format;
-	return (0);
-}
+    va_list    args;
+    int        printed_chars;
 
-int	main(void)
-{
-	return (0);
+    printed_chars = 0;
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%' && *(++format))
+        {
+            if (*format == 'c')
+                printed_chars += ft_putchar(va_arg(args, int), 1);
+            if (*format == 's')
+                printed_chars += ft_putstr(va_arg(args, char *), 1);
+            // if (*format == 'd')
+            //     printed_chars += ft_putnbr();
+        }
+        else
+            printed_chars += ft_putchar((char) *format, 1);
+        format++;
+    }
+    va_end(args);
+    return (printed_chars);
 }
--- src/util/util.c
+++ src/util/util.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   util.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anpicard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/14 11:25:38 by anpicard          #+#    #+#             */
-/*   Updated: 2024/11/14 17:31:54 by anpicard         ###   ########.fr       */
+/*   Created: 2024/11/14 15:49:05 by anpicard          #+#    #+#             */
+/*   Updated: 2024/11/14 17:31:44 by anpicard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdarg.h>
-# include <stdio.h> //remind to del
+int	ft_putchar(char c, int fd)
+{
+    ft_putchar_fd(c, fd);
+    return (1);
+}
 
-int	ft_printf(const char *format, ...);
-int	ft_putchar(char c, int fd);
-int	ft_putstr(char *str, int fd);
-
-#endif
+int	ft_putstr(char *str, int fd)
+{
+    if (!str)
+    {
+        ft_putstr("(null)", 1);
+        return (6);
+    }
+    ft_putstr_fd(str, fd);
+    return (ft_strlen(str));
+}
